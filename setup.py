d99a541500c9e96ab5654e6dcba16932293c220d
italocosilva <italocosilva@gmail.com>
2022-01-25 08:52:34 -0300

    1st commit, uploading files

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='italo-gbdist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['italo-gbdist'],
+      zip_safe=False)
